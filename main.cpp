e459ea2056dbd541036465d6512a75bcd54f4825
Tin Bašić <tin.basic22@gmail.com>
2024-06-05 11:07:37 +0200

    Resolve

--- main.cpp
+++ main.cpp
@@ -1,79 +1,79 @@
 #include "nwpwin.h"
 
-// prepare class ("STATIC") for a ship
+// : prepare class ("STATIC") for a ship
 
-class STATIC : public vsite::nwp::window
-{
-	public:std::string class_name() override { return "STATIC"; }
+class Static : public vsite::nwp::window {
+public:
+	std::string class_name() override {
+		return "Static";
+	}
 };
 
+static const int ship_size = 25;
+static const DWORD style = WS_CHILD | WS_VISIBLE | SS_CENTER;
 class main_window : public vsite::nwp::window
 {
 protected:
-	void on_left_button_down(POINT p) override { 
-		//create ship if it doesn't exist yet
-		// change current location
-		
-		location = p;
-		if (!ship)
-		{
-			ship.create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 0, p.x, p.y, 50, 50);
+	void on_left_button_down(POINT p) override {
+		// : create ship if it doesn't exist yet
+		// : change current location
+		poz = p;
+		if (!ship) {
+			ship.create(*this, style, "X", 0, p.x, p.y, ship_size, ship_size);
 		}
-		else {
-			SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+		else
+		{
+			SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
 		}
 	}
 	void on_key_up(int vk) override {
-		// mark ship (if exists) as "not moving"
-		
+		// : mark ship (if exists) as "not moving"
 		DWORD style = GetWindowLong(ship, GWL_STYLE);
 		style &= ~WS_BORDER;
 
-
-		if (ship) {
-			SetWindowLong(ship, GWL_STYLE, style);
-			SetWindowPos(ship, 0, 0, 0, 0, 0, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOZORDER);
-		}
+		SetWindowLong(ship, GWL_STYLE, style);
+		SetWindowPos(ship, 0, 0, 0, 0, 0, SWP_NOZORDER | SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOMOVE);
 	}
 	void on_key_down(int vk) override {
-		// if ship exists, move it depending on key and mark as "moving"
+		RECT window_border;
+		GetClientRect(*this, &window_border);
 
-		RECT rc;
-		GetClientRect(*this, &rc);
-		int speed = GetKeyState(VK_CONTROL) < 0 ? 20 : 5;
-		DWORD style1 = GetWindowLong(ship, GWL_STYLE);
+		int speed = GetAsyncKeyState(VK_CONTROL) ? 50 : 10;
 
-		switch (vk)
+		if (ship)
 		{
+			switch (vk)
+			{
 			case VK_UP:
-			location.y = max(0, location.y - speed);
-			break;
-
+				poz.y = max(poz.y - speed, window_border.top);
+				break;
 			case VK_DOWN:
-				location.y = min(rc.bottom - 50, location.y + speed);
+				poz.y = min(poz.y + speed, window_border.bottom - ship_size);
 				break;
-
 			case VK_LEFT:
-				location.x = max(0, location.x - speed);
+				poz.x = max(poz.x - speed, window_border.left);
 				break;
-
 			case VK_RIGHT:
-				location.x = min(rc.right - 50, location.x + speed);
+				poz.x = min(poz.x + speed, window_border.right - ship_size);
 				break;
-		
 			default:
 				return;
+			}
+
+			DWORD style = GetWindowLong(ship, GWL_STYLE);
+			style |= WS_BORDER;
+
+			SetWindowLong(ship, GWL_STYLE, style);
+			SetWindowPos(ship, 0, poz.x, poz.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
 		}
-		style1 |= WS_BORDER;
-		SetWindowLong(ship, GWL_STYLE, style1);
-		SetWindowPos(ship, 0, location.x, location.y, 0, 0, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOZORDER);
 	}
 	void on_destroy() override {
 		::PostQuitMessage(0);
 	}
 private:
-	STATIC ship;
-	POINT location;
+	Static ship;
+	POINT poz;
+
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
